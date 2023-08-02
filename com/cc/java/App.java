49f1642bffbd505f68ae08ed00934861f07dad42
Nico <nico.aldag@web.de>
2023-08-02 12:41:16 +0200

    Rectangle implementation

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,15 @@ public class App {
     
     public static void main(String[] args) {
      
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
        
+        output("--------Flächen--------");
+        output("Fläche Rechteck " + rectangle.area());
+
+        
+        
+        
     }
-
-  
-
-   
-
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
