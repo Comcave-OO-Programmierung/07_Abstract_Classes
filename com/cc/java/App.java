fc082638e7a7f0b6c645b01276bf8f01a6a24e41
Nico <nico.aldag@web.de>
2023-08-02 12:59:30 +0200

    Klassen erzeugt, Methoden implementiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,9 +5,13 @@ public class App {
     public static void main(String[] args) {
      
         Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
        
         output("--------Flächen--------");
         output("Fläche Rechteck " + rectangle.area());
+        output("Fläche Rechteck " + triangle.area());
+        output("Fläche Rechteck " + circle.area());
 
         
         
